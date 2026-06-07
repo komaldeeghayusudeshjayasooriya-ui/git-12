aa58bb174657a0e6f5904ebbcb658b830a30b276
komal jayasooriya <komaldeeghayusudeshjayasooriya2gmail.com>
2026-06-07 19:02:26 +0530

    this is a incorrect code

--- git12.c
+++ git12.c
@@ -2,21 +2,11 @@
 
 int main() {
    
-    printf("call of duty\n");
-    printf("game\n");
-    printf("donky\n");
-    printf("cat\n");
-    printf("lion\n");
-    printf("car\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-
-    printf("this is my last line\n");
-    printf("this is my last line\n");
-    printf("this is my last line\n");
+    printf("This is a bad code\n");
+    printf("This is a incorrect code\n");
+    printf("This is a bad code\n");
+    printf("This is a incorrectcode\n");
+    printf("This is a bad code\n");
 
 
 
